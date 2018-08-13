5db9219009935971342e9e237b001ac86502f6ac
Jay Vaughan <jay.vaughan@openresearch.com>
2018-08-13 21:18:39 +0200

    aliases for git sync

---
+++ git_aliases.sh
@@ -0,0 +1,13 @@
+
+alias g='git'
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias gfa='git fetch --all'
+alias ggo='git checkout '
+alias gh='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+alias gl='git log'
+alias gs='git status '
+alias gss='git for-each-ref --sort=-committerdate'
+
